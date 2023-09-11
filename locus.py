147704be979204f08503aad1cbe57300636916ba
Rahul-jpeg <rahulveer262@gmail.com>
2023-09-11 19:35:28 +0530

    resolved fernet key issue and decryption model

--- locus.py
+++ locus.py
@@ -2,46 +2,44 @@
 import os
 import json
 import base64
-import keyboard
 from cryptography.fernet import Fernet
 import msvcrt
 import http.server
 import socketserver
 import threading
 import signal
+from PyInquirer import prompt
+from dotenv import load_dotenv
+
 
 # Function to generate key
+def load_key():
+    load_dotenv()
+    key = os.getenv('SECRET')
+    return key
 
-
-def generate_key():
-    return Fernet.generate_key()
 
 # Function to encrypt the data
-
-
 def encrypt(text, key):
     fernet = Fernet(key)
     encrypted_text = fernet.encrypt(text.encode())
     return encrypted_text
 
+
 # Function to decrypt the data
-
-
 def decrypt(encrypted_text, key):
-    fernet_key = Fernet(base64.urlsafe_b64encode(key.encode()[:32]))
+    # fernet_key = Fernet(base64.urlsafe_b64encode(key.encode()[:32]))
+    fernet_key = Fernet(key)
     decrypted_text = fernet_key.decrypt(encrypted_text).decode()
     return decrypted_text
 
 
 # Function to check if the input key is unique
-
-
 def is_unique_key(master_dict, key):
     return key not in master_dict
 
+
 # Function to mask the input
-
-
 def mask_input(prompt):
     print(prompt, end='', flush=True)
     password = []
@@ -59,21 +57,19 @@ def mask_input(prompt):
             print('*', end='', flush=True)
     return ''.join(password)
 
+
 # Function to load previous data
-
-
 def load_master_dict():
     if os.path.exists("data.bin"):
         with open("data.bin", "rb") as f:
             binary_data = f.read()
-            decoded_data = base64.b64decode(binary_data).decode()
-            return json.loads(decoded_data)
-    else:
+            if binary_data:
+                decoded_data = base64.b64decode(binary_data).decode()
+                return json.loads(decoded_data)
         return {}
 
+
 # Function to store data
-
-
 def store_data(master_dict):
     # Update master_dict with existing data
     master_dict.update(load_master_dict())
@@ -87,7 +83,7 @@ def store_data(master_dict):
             continue
 
         # Encrypt the label and value
-        encryption_key = generate_key()
+        encryption_key = load_key()
         encrypted_label = encrypt(label, key=encryption_key)
         encrypted_value = encrypt(value, key=encryption_key)
 
@@ -116,13 +112,10 @@ def store_data(master_dict):
             f.write(binary_data)
 
         print("Data stored successfully.")
-        print("Your key: ", encryption_key)
         break
 
 
 # Function to retrieve data
-
-
 def retrieve_data(master_dict):
     while True:
 
@@ -146,7 +139,8 @@ def retrieve_data(master_dict):
                 break
 
         # Prompt the user for the key to decrypt
-        decryption_key = input("Enter the key to decrypt: ").strip()
+        # decryption_key = input("Enter the key to decrypt: ").strip()
+        decryption_key = load_key()
 
         # Retrieve the key-value pair and decrypt
         encrypted_label = list(master_dict[key].keys())[0]
@@ -166,58 +160,61 @@ def retrieve_data(master_dict):
 
 
 # Function to view the data in an HTML file
-
-
 def view_data(master_dict):
+    flag = False
     if os.path.exists("data.bin"):
         with open("data.bin", "rb") as f:
             binary_data = f.read()
-            decoded_data = base64.b64decode(binary_data).decode()
-            master_dict = json.loads(decoded_data)
+            if binary_data:
+                decoded_data = base64.b64decode(binary_data).decode()
+                master_dict = json.loads(decoded_data)
+                flag = True
+            else:
+                print("No items to view")
 
         # Create HTML content
-        html_content = "<html><body><h1>Stored Data:</h1><ul>"
-        for key, key_value_pair in master_dict.items():
-            encrypted_label = list(key_value_pair.keys())[0]
-            encrypted_value = key_value_pair[encrypted_label]
-            html_content += f"<li>Key: {key}, Label: {encrypted_label}, Value: {encrypted_value}</li>"
-        html_content += "</ul></body></html>"
+        if flag:
+            html_content = "<html><body><h1>Stored Data:</h1><ul>"
+            for key, key_value_pair in master_dict.items():
+                encrypted_label = list(key_value_pair.keys())[0]
+                encrypted_value = key_value_pair[encrypted_label]
+                html_content += f"<li><b>Key:</b> {key},<br> <b>Label:</b> {encrypted_label},<br> <b>Value:</b> {encrypted_value}</li>"
+            html_content += "</ul></body></html>"
 
-        # Write HTML content to file
-        with open("data.html", "w") as f:
-            f.write(html_content)
+            # Write HTML content to file
+            with open("data.html", "w") as f:
+                f.write(html_content)
 
-        print("HTML file created and hosted on localhost:6001")
+            print("HTML file created and hosted on localhost:6001")
 
-        class CustomHandler(http.server.BaseHTTPRequestHandler):
-            def do_GET(self):
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write(html_content.encode())
+            class CustomHandler(http.server.BaseHTTPRequestHandler):
+                def do_GET(self):
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    self.wfile.write(html_content.encode())
 
-        global server
+            global server
 
-        PORT = 6001
-        with socketserver.TCPServer(("", PORT), CustomHandler) as server:
-            print(f"Server running on localhost:{PORT}")
+            PORT = 6001
+            with socketserver.TCPServer(("", PORT), CustomHandler) as server:
+                print(f"Server running on localhost:{PORT}")
 
-            view_data.server = server
-            signal.signal(signal.SIGINT, stop_server)
+                view_data.server = server
+                signal.signal(signal.SIGINT, stop_server)
 
-            # Start the server in a separate thread
-            server_thread = threading.Thread(target=server.serve_forever)
-            server_thread.daemon = True
-            server_thread.start()
+                # Start the server in a separate thread
+                server_thread = threading.Thread(target=server.serve_forever)
+                server_thread.daemon = True
+                server_thread.start()
 
-            input("Press Enter to stop the server.")
+                input("Press Enter to stop the server.")
 
     else:
         print("No data found.")
 
+
 # Function to shutdown the server
-
-
 def stop_server(sig, frame):
     global server
     if server is not None:
@@ -225,37 +222,29 @@ def stop_server(sig, frame):
         server.shutdown()
         server = None
 
+
 # Function to clear screen on every iteration
-
-
 def clear_screen():
     os.system("cls" if os.name == "nt" else "clear")
 
+
 # Function to navigate through the options during selection
-
-
 def select_option():
-    options = ["Store", "Retrieve", "View"]
-    current_option = 0
-
-    while True:
-        clear_screen()
-
-        for i, option in enumerate(options):
-            if i == current_option:
-                print(f"> {option}")
-            else:
-                print(f"  {option}")
-
-        # Detect arrow key presses to navigate options
-        event = keyboard.read_event(suppress=True)
-        if event.event_type == keyboard.KEY_DOWN:
-            if event.name == "down":
-                current_option = (current_option + 1) % len(options)
-            elif event.name == "up":
-                current_option = (current_option - 1) % len(options)
-        elif event.event_type == keyboard.KEY_UP and event.name == "enter":
-            return options[current_option]
+    questions = [
+        {
+            'type': 'list',
+            'name': 'menu',
+            'message': 'Select an option:',
+            'choices': [
+                'Store',
+                'Retrieve',
+                'View',
+            ],
+        }
+    ]
+    answers = prompt(questions)
+    selected_option = answers['menu']
+    return (selected_option)
 
 
 def main():
